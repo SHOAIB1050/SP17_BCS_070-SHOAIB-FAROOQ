fc181b989216003fe5488756efc3fea7e19d8b1c
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-12-05 19:05:07 +0500

    Contact Us Screen

--- dis_app/lib/MainDrawer.dart
+++ dis_app/lib/MainDrawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -16,15 +17,66 @@ class MainDrawer extends StatelessWidget {
                   Container(
                     width: 100,
                     height: 100,
+                    margin: EdgeInsets.only(top: 30, bottom: 10),
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                        image: AssetImage('assets/imags/1.png'),
-                      ),
+                          image: AssetImage('assets/imags/2.png'),
+                          fit: BoxFit.fill),
                     ),
+                  ),
+                  Text(
+                    "SHOAIB FAROOQ",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  Text(
+                    "SP17_BCS_070",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
                   )
                 ],
               ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text(
+              "m.shoaib1050@gmail.com",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.school),
+            title: Text(
+              "COMSATS",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.work),
+            title: Text(
+              "Freelancer",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.voice_chat),
+            title: Text(
+              "m.shoaib1050",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text(
+              "+92310692XXX",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text(
+              "Burewala Pakistan",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
           )
         ],
