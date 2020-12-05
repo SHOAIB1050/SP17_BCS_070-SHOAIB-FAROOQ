c22e8c89abb656b5aff05796d2ec20f5becf1515
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-12-05 13:32:16 +0500

    levels added

--- dis_app/lib/HomePage.dart
+++ dis_app/lib/HomePage.dart
@@ -8,6 +8,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(40.0),
+            child: InkWell(
+              child: Material(
+                elevation: 20.0,
+                borderRadius: BorderRadius.circular(20.0),
+                child: Container(
+                  height: 250,
+                  width: 300,
+                  child: ClipOval(
+                    child: Image.asset('assets/imags/1.png'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: (Text(
+                'SIMPLE',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Satisfy'),
+              )),
+            ),
+            margin: EdgeInsets.all(30.0),
+            height: 60,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadiusDirectional.circular(20.0)),
+          ),
+          Container(
+            child: Center(
+              child: (Text(
+                'HARD',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Satisfy'),
+              )),
+            ),
+            margin: EdgeInsets.all(30.0),
+            height: 60,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadiusDirectional.circular(20.0)),
+          ),
+        ],
+      ),
+    );
   }
 }
